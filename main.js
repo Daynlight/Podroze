c7e08fc30ebb472c187798107912320d60f825be
daynlight <danielstodulski000@gmail.com>
2023-05-02 20:38:03 +0200

    Json

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ var height = window.innerHeight;
 var width = window.innerWidth;
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, width / height, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera( 75, width / height, 0.001, 1000 );
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setPixelRatio(window.setPixelRatio);
