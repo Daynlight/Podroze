20c87f859c0ad39a0c3590d70d10c1cd2f67ec2a
Daynlight <62157770+Daynlight@users.noreply.github.com>
2023-05-04 09:06:42 +0200

    Update Login.js

--- Login.js
+++ Login.js
@@ -8,7 +8,12 @@ function LoginFormChange(ModeLogin = false)
 	<input type="password" name="Password1" id="Password1" placeholder="Password" class="form">
 	<input type="button" id="submit" value="Register" class="form">
   	</form>
-	`
+	`;
+	
+	var LoginFormBTN =
+	`<div id="Login" onclick="LoginFormChange(true)" class="Login col-2 d-flex justify-content-center ChoseActive">Login</div>
+	<div id="Register" onclick="LoginFormChange(false)" class="Register col-2 d-flex justify-content-center">Register</div>`;
+	
 	var RegisterForm = 
 	`
 	<form>
@@ -19,17 +24,18 @@ function LoginFormChange(ModeLogin = false)
 	<input type="password" name="Password2" id="Password2" placeholder="Repeat Password" class="form">
 	<input type="button" id="submit" value="Register" class="form">
   	</form>
-	`
+	`;
+	
+	var RegisterFormBTN = 
+	`<div id="Login" onclick="LoginFormChange(true)" class="Login col-2 d-flex justify-content-center">Login</div>
+	<div id="Register" onclick="LoginFormChange(false)" class="Register col-2 d-flex justify-content-center ChoseActive">Register</div>`;
 
 	if(ModeLogin) {
 		document.getElementById("LoginForm").innerHTML = LoginForm;
-		document.getElementById("LoginChose").innerHTML = 
-		`<div id="Login" onclick="LoginFormChange(true)" class="Login col-2 d-flex justify-content-center ChoseActive">Login</div>
-		<div id="Register" onclick="LoginFormChange(false)" class="Register col-2 d-flex justify-content-center">Register</div>`;
+		document.getElementById("LoginChose").innerHTML = LoginFormBTN;
+		
 	} else {
 		document.getElementById("LoginForm").innerHTML = RegisterForm;
-		document.getElementById("LoginChose").innerHTML = 
-		`<div id="Login" onclick="LoginFormChange(true)" class="Login col-2 d-flex justify-content-center">Login</div>
-		<div id="Register" onclick="LoginFormChange(false)" class="Register col-2 d-flex justify-content-center ChoseActive">Register</div>`;
+		document.getElementById("LoginChose").innerHTML = RegisterFormBTN;
 	}
 }
